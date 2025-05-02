79b702548fa3e27813b602ae928e9c12553543f8
NoufAlwakiyel <142484727+NoufAlwakiyel@users.noreply.github.com>
2025-05-03 00:19:38 +0300

    member 2 tasks implemented

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
 import bleach
-
+# Noufs part 
+import hashlib  # For insecure password hashing (MD5)
+import sqlite3  # For raw SQL queries (vulnerable to injection)
+#end of the imports from noufs part
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'thisisasecretkey'
@@ -137,6 +140,57 @@ def register():
     return render_template('register.html', form=form)
 
 
+
+
+
+
+# Noufs part 
+
+# Vulnerable login (SQL Injection)
+@app.route('/login_vulnerable', methods=['GET', 'POST'])
+def login_vulnerable():
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        
+        # Hash the password using MD5 (insecure)
+        md5_password = hashlib.md5(password.encode()).hexdigest()
+        
+        # VULNERABLE to SQL Injection! 
+        conn = sqlite3.connect('instance/database.db')
+        cursor = conn.cursor()
+        query = f"SELECT * FROM user WHERE username = '{username}' AND password = '{md5_password}'"
+        
+        # Execute vulnerable query
+        result = cursor.execute(query).fetchone()
+        conn.close()
+        
+        if result:
+            user = User.query.get(result[0])  # Get user by ID
+            login_user(user)
+            return redirect(url_for('dashboard'))
+    
+    return render_template('login_vulnerable.html', form=form)
+
+# Register with vulnerable password storage (MD5 - insecure)
+@app.route('/register_vulnerable', methods=['GET', 'POST'])
+def register_vulnerable():
+    form = RegisterForm()
+    
+    if form.validate_on_submit():
+        # Hash password with MD5 (INSECURE!)
+        md5_password = hashlib.md5(form.password.data.encode()).hexdigest()
+        
+        new_user = User(username=form.username.data, password=md5_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login_vulnerable'))
+    
+    return render_template('register_vulnerable.html', form=form)
+
+
+# database creation
 if __name__ == "__main__":
     with app.app_context():
         db.drop_all()     # Deletes all tables and data
